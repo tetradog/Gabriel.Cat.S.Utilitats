c187b3f4e66685e28101855ee3f4aed1046da2f7
tetradog <tetradog@gmail.com>
2018-03-13 05:18:46 +0100

    desarrollando la versión standar de la dll Utilitats de momento subo el proyecto y la primera clase :)

---
+++ Gabriel.Cat.S.Utilitats/Llista.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Gabriel.Cat.S.Utilitats
+{
+    public class Llista<T> : IList, IList<T>
+    {
+        List<T> list;
+        Semaphore semaphore;
+        public Llista(IEnumerable<T> initialElements = null)
+        {
+            list = new List<T>();
+            semaphore = new Semaphore(1, 1);
+            if (initialElements != null)
+                list.AddRange(initialElements);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                T value;
+                try
+                {
+                    semaphore.WaitOne();
+                    value = list[index];
+
+                }
+                catch { throw; }
+                finally
+                {
+                    semaphore.Release();
+                }
+                return value;
+            }
+            set
+            {
+
+                try
+                {
+                    semaphore.WaitOne();
+                    list[index] = value;
+
+                }
+                catch { throw; }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }
+        }
+
+        object IList.this[int index] { get => this[index]; set => this[index] = (T)value; }
+
+        public int Count
+        {
+            get
+            {
+                int count;
+                try
+                {
+                    semaphore.WaitOne();
+                    count = list.Count;
+
+                }
+                catch { throw; }
+                finally
+                {
+                    semaphore.Release();
+                }
+                return count;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        bool IList.IsFixedSize => false;
+
+        bool IList.IsReadOnly => IsReadOnly;
+
+        int ICollection.Count => Count;
+
+        bool ICollection.IsSynchronized
+        {
+            get
+            {
+                bool isSynchronized;
+                try
+                {
+                    semaphore.WaitOne();
+                    isSynchronized = ((IList)list).IsSynchronized;
+                }
+                catch { throw; }
+                finally
+                {
+                    semaphore.Release();
+                }
+                return isSynchronized;
+            }
+        }
+
+        object ICollection.SyncRoot
+        {
+            get
+            {
+                object syncRoot;
+                try
+                {
+                    semaphore.WaitOne();
+                    syncRoot = ((IList)list).SyncRoot;
+                }
+                catch { throw; }
+                finally
+                {
+                    semaphore.Release();
+                }
+                return syncRoot;
+            }
+        }
+
+        public void Add(T item)
+        {
+            try
+            {
+                semaphore.WaitOne();
+                list.Add(item);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public void Clear()
+        {
+            try
+            {
+                semaphore.WaitOne();
+                list.Clear();
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            bool contains;
+            try
+            {
+                semaphore.WaitOne();
+                contains = list.Contains(item);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            return contains;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            try
+            {
+                semaphore.WaitOne();
+                list.CopyTo(array, arrayIndex);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            IEnumerator<T> enumerator;
+            try
+            {
+                semaphore.WaitOne();
+                enumerator = list.GetEnumerator();
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            return enumerator;
+        }
+
+        public int IndexOf(T item)
+        {
+            int indexOf;
+            try
+            {
+                semaphore.WaitOne();
+                indexOf = list.IndexOf(item);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            return indexOf;
+        }
+
+        public void Insert(int index, T item)
+        {
+       
+            try
+            {
+                semaphore.WaitOne();
+                list.Insert(index,item);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            
+        }
+
+        public bool Remove(T item)
+        {
+            bool removed;
+            try
+            {
+                semaphore.WaitOne();
+                removed = list.Remove(item);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            return removed;
+        }
+
+        public void RemoveAt(int index)
+        {
+            
+            try
+            {
+                semaphore.WaitOne();
+                list.RemoveAt(index);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+           
+        }
+
+        int IList.Add(object value)
+        {
+            int pos;
+            try
+            {
+                semaphore.WaitOne();
+                pos = ((IList)list).Add(value);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+            return pos;
+        }
+
+        void IList.Clear()
+        {
+            Clear();
+        }
+
+        bool IList.Contains(object value)
+        {
+            return Contains((T)value);
+        }
+
+        void ICollection.CopyTo(Array array, int index)
+        {
+           
+            try
+            {
+                semaphore.WaitOne();
+                ((IList)list).CopyTo(array,index);
+
+            }
+            catch { throw; }
+            finally
+            {
+                semaphore.Release();
+            }
+     
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        int IList.IndexOf(object value)
+        {
+            return IndexOf((T)value);
+        }
+
+        void IList.Insert(int index, object value)
+        {
+            Insert(index, (T)value);
+        }
+
+        void IList.Remove(object value)
+        {
+            Remove((T)value);
+        }
+
+        void IList.RemoveAt(int index)
+        {
+            RemoveAt(index);
+        }
+
+    }
+}
