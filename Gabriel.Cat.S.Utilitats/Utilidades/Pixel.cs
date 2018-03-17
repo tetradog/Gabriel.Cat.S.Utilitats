2b06aea936ce49ca1964545ae2c3791a0b77ef82
tetradog <tetradog@gmail.com>
2018-03-17 05:42:02 +0100

    subir archivo

---
+++ Gabriel.Cat.S.Utilitats/Utilidades/Pixel.cs
@@ -0,0 +1,11 @@
+﻿namespace Gabriel.Cat.S.Drawing
+{
+    public class Pixel
+    {
+        public const int R = 2;
+        public const int G = 1;
+        public const int B = 0;
+        public const int A = 3;
+    }
+
+}
