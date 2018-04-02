42643d8c6806c2b64bbe8ca4c29bf4eaf0ad02fc
tetradog <tetradog@gmail.com>
2018-04-02 21:44:47 +0200

    añadiendo metodos

---
+++ Gabriel.Cat.S.Utilitats/Extension/ExtensionTimeSpan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.Extension
+{
+    public static class ExtensionTimeSpan
+    {
+        public static string ToHoursMinutesSeconds(this TimeSpan time,bool addMiliseconds=false)
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(time.Hours);
+            str.Append(":");
+            str.Append(time.Minutes);
+            str.Append(":");
+            str.Append(time.Seconds);
+            if(addMiliseconds)
+            {
+                str.Append(".");
+                str.Append(time.Milliseconds);
+            }
+            return str.ToString();
+        }
+    }
+}
