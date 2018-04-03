2635d523c9221eae922bf33e6a9f5c4cdfe28850
tetradog <tetradog@gmail.com>
2018-04-03 17:27:25 +0200

    pensando en los nullable<T>...los pongo en una clase aparte?

--- Gabriel.Cat.S.Utilitats/Extension/ExtensionIComparable.cs
+++ Gabriel.Cat.S.Utilitats/Extension/ExtensionIComparable.cs
@@ -12,6 +12,12 @@ namespace Gabriel.Cat.S.Extension
             const int INFERIOR = -1;
             return left == null && right == null ? IGUALES : left != null ? left.CompareTo(right) : INFERIOR;
         }
+        public static int CompareTo<T>(this Nullable<T> left,Nullable<T> right) where T :struct, IComparable
+        {
+            const int IGUALES = 0;
+            const int INFERIOR = -1;
+            return !left.HasValue&& !right.HasValue? IGUALES : left.HasValue ? left.Value.CompareTo(right.Value) : INFERIOR;
+        }
         public static SortedList<T,T> ToSortedList<T>(this IList<T> lst) where T : IComparable
         {
             SortedList<T, T> sortedList = new SortedList<T, T>();
