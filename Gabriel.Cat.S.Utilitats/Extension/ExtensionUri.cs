fd288e6bc448e23f1f53984b44eb4819fd048d22
Gabriel <tetradog@gmail.com>
2021-07-11 06:03:00 +0200

    añadiendo metodos de extension

--- Gabriel.Cat.S.Utilitats/Extension/ExtensionUri.cs
+++ Gabriel.Cat.S.Utilitats/Extension/ExtensionUri.cs
@@ -5,12 +5,14 @@ using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gabriel.Cat.S.Extension
 {
     public static class ExtensionUri
     {
+        static Semaphore smDownloading = new Semaphore(1, 1);
         public static Process Abrir([NotNull]this Uri url)
         {//source https://github.com/dotnet/wpf/issues/2566
             return Process.Start(new ProcessStartInfo
@@ -57,6 +59,37 @@ namespace Gabriel.Cat.S.Extension
 
             return response;
         }
+       
+        public static async Task<string> DownloadStringAsync(this Uri url)
+        {
+            string result = string.Empty;
+            Task download = new Task(new Action(() => result = url.DownloadString()));
+            await download;
+            return result;
+        }
+        public static string DownloadString(this Uri url)
+        {
+            string html;
+            WebClient wbClient;
+
+            try
+            {
+                smDownloading.WaitOne();
+                wbClient = new WebClient();
+                wbClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                html = wbClient.DownloadString(url);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                smDownloading.Release();
+            }
+            return html;
+        }
+
     }
 
 }
