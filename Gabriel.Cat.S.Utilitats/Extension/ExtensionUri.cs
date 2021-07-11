7095af65d0d7b874d9e16d71716c12b5252fb750
Gabriel <tetradog@gmail.com>
2021-07-11 05:52:00 +0200

    añadiendo metodos de extension

--- Gabriel.Cat.S.Utilitats/Extension/ExtensionUri.cs
+++ Gabriel.Cat.S.Utilitats/Extension/ExtensionUri.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Gabriel.Cat.S.Extension
 {
     public static class ExtensionUri
     {
-        public static Process Abrir(this Uri url)
+        public static Process Abrir([NotNull]this Uri url)
         {//source https://github.com/dotnet/wpf/issues/2566
             return Process.Start(new ProcessStartInfo
            {
@@ -15,8 +19,44 @@ namespace Gabriel.Cat.S.Extension
                WindowStyle = ProcessWindowStyle.Hidden,
                UseShellExecute = false,
                CreateNoWindow = true,
-               Arguments = $"/c start {url.AbsoluteUri}"
+               Arguments = $"/c start \"{url.AbsoluteUri}\""
            });
         }
+        public static Bitmap GetBitmap([NotNull] this Uri url)
+        {
+            WebRequest request = WebRequest.Create(url.AbsoluteUri);
+            WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream = response.GetResponseStream();
+            return new Bitmap(responseStream);
+        }
+        public static HttpStatusCode GetStatusCode([NotNull] this Uri url)
+        {
+            HttpStatusCode response; 
+            HttpWebRequest httpReq;
+            HttpWebResponse httpRes=default;
+            
+            try
+            {
+                httpReq = (HttpWebRequest)WebRequest.Create(url.AbsoluteUri);
+
+                httpReq.AllowAutoRedirect = false;
+                httpRes = (HttpWebResponse)httpReq.GetResponse();
+
+                response = httpRes.StatusCode;
+               
+            }
+            catch
+            {
+                response = HttpStatusCode.NotFound;
+            }
+            finally
+            {
+                if(!Equals(httpRes, default))
+                    httpRes.Close();
+            }
+
+            return response;
+        }
     }
+
 }
